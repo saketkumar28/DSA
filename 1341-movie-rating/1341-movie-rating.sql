256d828ddfb3e222c758ab126dd3d8837a6362f1
Saket kumar <kumarsaket2801@gmail.com>
2026-09-20 13:32:23 +0530

    Time: 1500 ms (53.26%), Space: 0B (100%) - LeetHub

---
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -0,0 +1,2 @@
+(select name as results from MovieRating mr join Users u on mr.user_id=u.user_id group by u.user_id order by count(u.user_id) desc,name asc limit 1) union all
+(select title as results from MovieRating mr join Movies m on mr.movie_id=m.movie_id where mr.created_at>='2020-02-01' and mr.created_at<='2020-02-29' group by mr.movie_id order by avg(mr.rating) desc,title asc limit 1);
