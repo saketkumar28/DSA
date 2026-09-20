9dbf7c1fbfcdfddb02877a04a94499c0ce97e53a
Saket kumar <kumarsaket2801@gmail.com>
2026-09-20 17:09:31 +0530

    Time: 606 ms (63.17%), Space: 0B (100%) - LeetHub

---
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+select distinct num as ConsecutiveNums from (
+    select num,lead(num,1) over (order by id) as next1,lead(num,2) over (order by id) as next2 from logs
+) t
+where num=next1 and num=next2;
